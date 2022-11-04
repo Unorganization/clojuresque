c3f9938474cea20c851fcdec74ca0cac28e2ba8c
dev <dev@dev.com>
2022-11-04 11:35:01 -0700

    .

--- mk_uber.bat
+++ mk_uber.bat
@@ -1,10 +1,16 @@
+rem # for Mac OSX
+rem sudo xattr -r -d com.apple.quarantine ${GRAALVM_HOME}
+rem gu install native-image
+
+rem https://github.com/clj-easy/graalvm-clojure
+
 rem C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\Common7\Tools\vcvars64.bat
 rem C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat
 rem C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat
 
 
-lein uberjar  
-"C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat" 
-set JAVA_HOME=C:\Program Files\GraalVM\graalvm-ce-java17-22.3.0 
+rem lein uberjar  
+rem "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat" 
+rem set JAVA_HOME=C:\Program Files\GraalVM\graalvm-ce-java17-22.3.0 
 
-native-image --no-fallback -jar target\default+uberjar\clojuresque-0.1.0-SNAPSHOT-standalone.jar 
+rem native-image --no-fallback -jar target\default+uberjar\clojuresque-0.1.0-SNAPSHOT-standalone.jar 
