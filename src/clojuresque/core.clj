9e8d450c0466c3bf774a67f7bff498f8fdd6863e
dev <dev@dev.com>
2022-11-13 19:50:52 -0800

    package

--- src/clojuresque/core.clj
+++ src/clojuresque/core.clj
@@ -1,10 +1,12 @@
 (ns clojuresque.core
   (:require [clojuresque.greet :as greet])
   (:require [clojuresque.agent-try :as agent-try])
+  (:require [cheshire.core :as cheshire])
   (:require [clojuresque.all :as all])
   (:gen-class))
 
-(defn -main
+
+  (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println (greet/hello-world-msg))
@@ -13,4 +15,5 @@
   (agent-try/agent-ex)
   (println "after agent")
   
+  (println (cheshire/generate-string {:name "kenny" :mood "happy"}))
   )
